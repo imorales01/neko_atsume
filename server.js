be5e28352a19ee2ac71b5fb39d97ad9da1078f8b
Ivelisse Morales <imorales315@gmail.com>
2018-06-19 15:41:10 -0400

    Template for database connection

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+//DB config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+/*
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+*/
