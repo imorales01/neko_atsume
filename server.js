24cec4cc36b688ad8d340b84bc4e7b0c4374eea5
Ivelisse Morales <imorales315@gmail.com>
2018-06-19 15:42:00 -0400

    Runs server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+
 //DB config
 const db = require("./config/keys").mongoURI;
 
@@ -12,3 +13,9 @@ mongoose
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 */
+
+app.get("/", (req, res) => res.send("Hello!"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
