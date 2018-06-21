4d5088ea305b0298ccd16c3e239d9b57c9f6f8d0
Maria Volpe <mariaCHVolpe@gmail.com>
2018-06-20 23:27:05 -0400

    Make menu buttons clickable

--- client/src/components/menu_interface.js
+++ client/src/components/menu_interface.js
@@ -5,15 +5,41 @@ import YardButton from '../img/UI/img_yard.png';
 import SettingsButton from '../img/UI/img_settings.png';
 
 class MenuInterface extends Component {
+	constructor(props){
+		super(props);
+
+		this.handleClickCats = this.handleClickCats.bind(this);
+		this.handleClickShop = this.handleClickShop.bind(this);
+		this.handleClickYard = this.handleClickYard.bind(this);
+		this.handleClickSettings = this.handleClickSettings.bind(this);
+
+	}
+	handleClickCats(){
+
+	}
+	handleClickShop(){
+		
+	}
+	handleClickYard(){
+		
+	}
+	handleClickSettings(){
+		
+	}
 	render() {
+		//render buttons on menu
+		// CatsButton -> Cats Collection
+		// ShopButton -> Shop
+		// YardButton -> Close menu
+		// SettingsButton -> will be converted to logout
 		return (
 			<div>
 			<table className="interface-container">
-				<img src={CatsButton} className='button'/>
-				<img src={ShopButton} className='button' />
+				<img src={CatsButton} className='button' onClick={this.handleClickCats}/>
+				<img src={ShopButton} className='button' onClick={this.handleClickShop}/>
 				<br />
-				<img src={YardButton} className='button'/>
-				<img src={SettingsButton} className='button' />
+				<img src={YardButton} className='button'onClick={this.handleClickYard}/>
+				<img src={SettingsButton} className='button'onClick={this.handleClickSettings} />
 			</table>
 			</div>
 		);
