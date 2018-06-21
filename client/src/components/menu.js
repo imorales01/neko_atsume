5c86bd27f480c987b262e3faf4b6e54fd849b239
Maria Volpe <mariaCHVolpe@gmail.com>
2018-06-20 23:33:16 -0400

    Comment and lint: 4 space tabs to 2 spaces

--- client/src/components/menu.js
+++ client/src/components/menu.js
@@ -4,28 +4,30 @@ import MenuButton from '../img/UI/img_menu.png';
 import CloseButton from '../img/UI/img_close.png';
 
 class Menu extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			menuOpen: false
-		};
+    this.state = {
+      menuOpen: false
+    };
 
-		this.handleClick = this.handleClick.bind(this);
-	}
-	handleClick(){
-		this.setState((prevState) => ({ menuOpen: !prevState.menuOpen }));
-	}
-	render() {
-		return (
-			<div>
-				<img src={this.state.menuOpen ? CloseButton : MenuButton} onClick={this.handleClick} style={{ height: "10%", width: "5%" }}/>
-				<div className='menu'>
-					{this.state.menuOpen ? <MenuInterface/> : <div /> }
-				</div>
-			</div>
-		);
-	}
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState((prevState) => ({ menuOpen: !prevState.menuOpen }));
+  }
+  render() {
+    //render menu or close button in corner
+    //render menu interface if menu is open
+    return (
+      <div>
+        <img src={this.state.menuOpen ? CloseButton : MenuButton} onClick={this.handleClick} style={{ height: "10%", width: "5%" }} />
+        <div className='menu'>
+          {this.state.menuOpen ? <MenuInterface /> : <div />}
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Menu;
--- client/src/components/menu_interface.js
+++ client/src/components/menu_interface.js
@@ -5,45 +5,45 @@ import YardButton from '../img/UI/img_yard.png';
 import SettingsButton from '../img/UI/img_settings.png';
 
 class MenuInterface extends Component {
-	constructor(props){
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.handleClickCats = this.handleClickCats.bind(this);
-		this.handleClickShop = this.handleClickShop.bind(this);
-		this.handleClickYard = this.handleClickYard.bind(this);
-		this.handleClickSettings = this.handleClickSettings.bind(this);
+    this.handleClickCats = this.handleClickCats.bind(this);
+    this.handleClickShop = this.handleClickShop.bind(this);
+    this.handleClickYard = this.handleClickYard.bind(this);
+    this.handleClickSettings = this.handleClickSettings.bind(this);
 
-	}
-	handleClickCats(){
+  }
+  handleClickCats() {
 
-	}
-	handleClickShop(){
-		
-	}
-	handleClickYard(){
-		
-	}
-	handleClickSettings(){
-		
-	}
-	render() {
-		//render buttons on menu
-		// CatsButton -> Cats Collection
-		// ShopButton -> Shop
-		// YardButton -> Close menu
-		// SettingsButton -> will be converted to logout
-		return (
-			<div>
-			<table className="interface-container">
-				<img src={CatsButton} className='button' onClick={this.handleClickCats}/>
-				<img src={ShopButton} className='button' onClick={this.handleClickShop}/>
-				<br />
-				<img src={YardButton} className='button'onClick={this.handleClickYard}/>
-				<img src={SettingsButton} className='button'onClick={this.handleClickSettings} />
-			</table>
-			</div>
-		);
-	}
+  }
+  handleClickShop() {
+
+  }
+  handleClickYard() {
+
+  }
+  handleClickSettings() {
+
+  }
+  render() {
+    //render buttons on menu
+    // CatsButton -> Cats Collection
+    // ShopButton -> Shop
+    // YardButton -> Close menu
+    // SettingsButton -> will be converted to logout
+    return (
+      <div>
+        <table className='interface-container'>
+          <img src={CatsButton} className='button' onClick={this.handleClickCats} />
+          <img src={ShopButton} className='button' onClick={this.handleClickShop} />
+          <br />
+          <img src={YardButton} className='button' onClick={this.handleClickYard} />
+          <img src={SettingsButton} className='button' onClick={this.handleClickSettings} />
+        </table>
+      </div>
+    );
+  }
 };
 
 export default MenuInterface;
