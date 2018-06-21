1068b6595e26171c73564fce17c6625acd5f116f
Maria Volpe <mariaCHVolpe@gmail.com>
2018-06-21 00:13:48 -0400

    Add header images to CatAlbum component

--- client/src/components/cat_album.js
+++ client/src/components/cat_album.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import Background from '../img/UI/TileBGs/cat_tile.png';
+import Title from '../img/UI/Titles/cat_title.png';
+import MenuButton from '../img/UI/img_menu.png';
+
+const catStyle = {
+	backgroundImage: `url(${Background})`,
+	backgroundPosition: 'center',
+  };
 
 class CatAlbum extends Component {
+	constructor(props){
+		super(props);
+	}
+
 	render() {
 		return (
-			
+			<div style={catStyle}>
+				<img src={MenuButton} onClick={ () => {} } style={{ height: "10%", width: "5%" }} />
+				<img src={Title} className='title' />
+			</div>
 		);
 	}
 };
