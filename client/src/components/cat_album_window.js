2177aeb98d17562c697679d36d75dbb39d967efe
Maria Volpe <mariaCHVolpe@gmail.com>
2018-06-21 11:04:07 -0400

    Change CatAlbumWindow component to be a div with a window background image

--- client/src/components/cat_album_window.js
+++ client/src/components/cat_album_window.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import AlbumWindowImg from '../img/UI/Windows/cat_album.png';
 
 // hard code height for now, depending on how big window container will be
 const windowStyle = {
-  height: "250px",
+  backgroundImage: `url(${AlbumWindowImg})`,
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: '100% 100%',
+  height: '250px',
+  width: '230px'
 };
-
 
 class CatAlbumWindow extends Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
   }
-
+  handleClick(){
+    
+  }
   render() {
     return (
-      <div>
-        <img src={AlbumWindowImg} style={windowStyle} />
+      <div style={windowStyle}>
+        <button onClick={this.handleClick}> Load! </button>
       </div>
     );
   }
