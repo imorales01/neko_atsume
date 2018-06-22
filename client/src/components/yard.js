8dde77884a96b25ad9b76cd535e63aa4cc7f4290
Maria Volpe <mariaCHVolpe@gmail.com>
2018-06-22 11:24:12 -0400

    Toggle HotSpotContainer component with MenuInterface component upon clicking menu button on yard

--- client/src/components/yard.js
+++ client/src/components/yard.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import Background from '../img/background.jpg';
 import Menu from './menu';
-import HotSpot from './hotspot';
+import HotSpotContainer from './hotspotcontainer';
 import CatAlbum from './cat_album';
+import MenuInterface from './menu_interface';
+import MenuButton from '../img/UI/img_menu.png';
+import CloseButton from '../img/UI/img_close.png';
 
 const yardStyle = {
   margin: '0',
@@ -15,10 +18,36 @@ const yardStyle = {
 };
 
 class Yard extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      menuOpen: false
+    }
+
+    this.handleMenuClick = this.handleMenuClick.bind(this);
+    this.showInterface = this.showInterface.bind(this);
+  }
+
+  handleMenuClick() {
+    this.setState((prevState) => ({ menuOpen: !prevState.menuOpen }));
+  };
+
+  showInterface() {
+    var open = this.state.menuOpen;
+    if(open) {
+      return (
+          <MenuInterface />
+      );
+    }
+  };
+
   render() {
     return (
       <div className='background-image' style={yardStyle}>
-        <Menu />
+      <img className='menu-button' src={ this.state.menuOpen ? CloseButton : MenuButton } alt='open or close interface' onClick={this.handleMenuClick} style={{ height: "10%", width: "10%" }} />
+      { this.state.menuOpen ? <MenuInterface /> : <HotSpotContainer /> }
+        
       </div>
     );
     // testing purposes:
