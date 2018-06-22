67e0cc70f9b75962ea2209d185e0b0e5c0f0e966
Maria Volpe <mariaCHVolpe@gmail.com>
2018-06-22 11:59:06 -0400

    Add hotspot to yard and style to become visible on hover and be clickable

--- client/src/components/hotspot.js
+++ client/src/components/hotspot.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import PlaceDownButton from '../img/UI/place.png';
+//import PlaceHolderButton from '../img/UI/transparent.png';
+
+const hotStyle = {
+  margin: "auto",
+  height: "20%",
+  width: "20%"
+}
 
 class HotSpot extends Component {
-	render() {
-		return (
-            null
-		);
-	}
+  constructor(props){
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+  onClick(){
+    alert("hello");
+  }
+  render() {
+    return (
+      <div>
+        <img src={PlaceDownButton} onClick={this.onClick} className='hotspot' />
+      </div>
+    );
+  }
 };
 
 export default HotSpot;
