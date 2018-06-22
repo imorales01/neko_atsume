2322fc43def02e7379bdf53066c9c80312446d51
Maria Volpe <mariaCHVolpe@gmail.com>
2018-06-22 12:50:12 -0400

    Clicking hotspot adds a random cat image to map

--- client/src/components/hotspot.js
+++ client/src/components/hotspot.js
@@ -11,15 +11,25 @@ const hotStyle = {
 class HotSpot extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      isEmpty: true,
+      randNum: null,
+      namePath: null
+    };
     this.onClick = this.onClick.bind(this);
   }
   onClick(){
-    alert("hello");
+    const randNum = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+    const randNum2 = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
+    const names = ['Willie', 'Shadow', 'Peaches', 'Rascal'];
+    const namePath = names[randNum2];
+    this.setState(() => ({ namePath, randNum, isEmpty: false }));
   }
   render() {
     return (
       <div>
-        <img src={PlaceDownButton} onClick={this.onClick} className='hotspot' />
+          { this.state.isEmpty ? <img src={PlaceDownButton} onClick={this.onClick} className='hotspot' /> :
+            <img src={require(`../img/cats/${this.state.namePath}/${this.state.randNum}.gif`)} className='cat' />} 
       </div>
     );
   }
