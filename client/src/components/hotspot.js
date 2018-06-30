02a0d2036625d950c928f5c006c4885ea6cf8e96
Maria Volpe <mariaCHVolpe@gmail.com>
2018-06-29 22:48:33 -0400

    Layer images to pair cats and toys

--- client/src/components/hotspot.js
+++ client/src/components/hotspot.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import PlaceDownButton from '../img/UI/place.png';
 
-const catStyle = {
-  paddingLeft: '150px',
-  paddingTop: '160px'
+const cat = {
+  position: 'absolute',
+  height: '30%'
 }
 
 class HotSpot extends Component {
@@ -13,6 +13,7 @@ class HotSpot extends Component {
       isEmpty: true,
       randNum: null,
       namePath: null,
+      toy: null,
       hotStyle: {
         paddingLeft: this.props.paddingLeft,
         paddingTop: this.props.paddingTop
@@ -26,11 +27,31 @@ class HotSpot extends Component {
   }
   // FIXME: dont hard code this
   onClick() {
-    const randNum = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-    const randNum2 = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
-    const names = ['Willie', 'Shadow', 'Peaches', 'Rascal'];
-    const namePath = names[randNum2];
-    this.setState(() => ({ namePath, randNum, isEmpty: false }));
+    // const randNum = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+    // const randNum2 = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
+    // const names = ['Willie', 'Shadow', 'Peaches', 'Rascal'];
+    // const namePath = names[randNum2];
+    // this.setState(() => ({ namePath, randNum, isEmpty: false }));
+    
+    //get toy img
+    //get cat img that corresponds
+    // types of toys
+    // const names = ['balls', 'pillows'];
+    // const pillows = ['mochi', 'sheep', 'beige'];
+    // const balls = ['redball'];
+    //const randType = names[Math.floor(Math.random() * ((names.length-1) - 0 + 1)) + 0];
+
+    const types = ['balls', 'pillows'];
+    const toys = { 'balls': ['redball'], 'pillows': ['mochi', 'sheep', 'beige'] };
+
+
+    const randType = types[Math.floor(Math.random() * ((types.length-1) - 0 + 1)) + 0];
+    const randToy = toys[randType][Math.floor(Math.random() * ((toys[randType].length-1) - 0 + 1)) + 0];
+    
+
+
+    this.setState(() => ({ namePath: randType, randNum: 1, toy: randToy, isEmpty: false }));
+
   }
   render() {
     // TODO: remove onClick for the cat picture. thats there for testing purposes only
@@ -44,13 +65,25 @@ class HotSpot extends Component {
                 className="hotspot"
                 style={this.state.hotStyle}
               />
-            : <img
-                src={require(`../img/cats/${this.state.namePath}/${this.state.randNum}.gif`)}
-                alt={`A picture of ${namePath}`}
-                className="cat"
-                style={this.state.catStyle}
-                onClick={this.onClick}
-              />
+            : 
+            <div>
+              <div style={cat}>
+                <img
+                  src={require(`../img/toys_cats/${this.state.namePath}/${this.state.randNum}.gif`)}
+                  alt={`A picture of ${this.state.namePath}`}
+                  style={this.state.catStyle}
+                  onClick={this.onClick}
+                />
+              </div>
+              <div style={cat}>
+                <img
+                  src={require(`../img/toys_cats/${this.state.namePath}/${this.state.toy}.gif`)}
+                  alt={`A picture of ${this.state.namePath}`}
+                  style={this.state.catStyle}
+                  onClick={this.onClick}
+                />
+              </div>
+            </div>
         }
       </div>
     );
