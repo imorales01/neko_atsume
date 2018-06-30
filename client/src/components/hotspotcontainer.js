60168a1e061bf69b544b889b683fb85c615da1d6
Maria Volpe <mariaCHVolpe@gmail.com>
2018-06-30 00:46:51 -0400

    Add more hotspots for a total of 6

--- client/src/components/hotspotcontainer.js
+++ client/src/components/hotspotcontainer.js
@@ -23,25 +23,76 @@ class HotSpotContainer extends Component {
     // TODO: px -> % and add more hot spots
     return (
       <div style={{ height: '100%' }}>
-        <div style={{position: 'absolute'}}>
-        <img
-          className="menu-button"
-          src={this.state.menuOpen ? null : MenuButton}
-          alt="Open or close menu"
-          onClick={this.handleMenuClick}
-          style={{ height: '10%', width: '10%' }}
-        />
+
+        <div style={{ position: 'absolute' }}>
+          <img
+            className="menu-button"
+            src={this.state.menuOpen ? null : MenuButton}
+            alt="Open or close menu"
+            onClick={this.handleMenuClick}
+            style={{ height: '10%', width: '10%' }}
+          />
         </div>
-        <div style={{position: 'absolute', height: '100%', width: '100%'}}>
-        <HotSpot
-          hotStyle={{
-            position: 'relative',
-            left: '4%',
-            top: '36%'
-          }}
-        />
+        <div style={{ position: 'absolute', height: '100%', width: '100%' }}>
+          <HotSpot
+            hotStyle={{
+              position: 'relative',
+              left: '4%',
+              top: '36%',
+              zIndex: '1'
+            }}
+          />
         </div>
-        
+        <div style={{ position: 'absolute', height: '100%', width: '100%' }}>
+          <HotSpot
+            hotStyle={{
+              position: 'relative',
+              left: '3%',
+              top: '65%',
+              zIndex: '3'
+            }}
+          />
+        </div>
+        <div style={{ position: 'absolute', height: '100%', width: '100%' }}>
+          <HotSpot
+            hotStyle={{
+              position: 'relative',
+              left: '35%',
+              top: '72%',
+              zIndex: '3'
+            }}
+          />
+        </div>
+        <div style={{ position: 'absolute', height: '100%', width: '100%' }}>
+          <HotSpot
+            hotStyle={{
+              position: 'relative',
+              left: '65%',
+              top: '50%',
+              zIndex: '4'
+            }}
+          />
+        </div>
+        <div style={{ position: 'absolute', height: '100%', width: '100%' }}>
+          <HotSpot
+            hotStyle={{
+              position: 'relative',
+              left: '75%',
+              top: '22%',
+              zIndex: '5'
+            }}
+          />
+        </div>
+        <div style={{ position: 'absolute', height: '100%', width: '100%' }}>
+          <HotSpot
+            hotStyle={{
+              position: 'relative',
+              left: '75%',
+              top: '84%',
+              zIndex: '6'
+            }}
+          />
+        </div>
       </div>
     );
   }
