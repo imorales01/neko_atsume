d6266a1844003124ce09b7ccfffb66363ff26efc
Maria Volpe <mariachvolpe@gmail.com>
2018-07-10 15:20:05 -0400

    Outline tests for menu interface

---
+++ client/src/test/menu-interface.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow, mount, render } from 'enzyme';
+import menu_interface from '../menu_interface';
+
+// test rendering of components
+describe('', () => {
+  it('should render without throwing an error', () => {
+    //expect<shallow>
+  });
+});
+
+// test links on buttons
+describe('', () => {
+  it('should build cat album component', () => {
+
+  });
+  it('should build shop component', () => {
+
+  });
+  // it('should build inventory component', () => {
+
+  // });
+  it('should go to yard', () => {
+
+  });
+  // it('should build settings component', () => {
+
+  // });
+});
