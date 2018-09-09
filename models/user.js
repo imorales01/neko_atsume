4db49b83cf39477f89c2339d7bcb9d8253d26dd0
Maria Volpe <mariaCHVolpe@gmail.com>
2018-09-09 03:09:51 -0400

    Update item schema and clean up schemas

--- models/user.js
+++ models/user.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-// define user schema
 const userSchema = new Schema({
   userID: String,
   username: String,
   catCollection: [{ catID: Number, dateCollected: String }]
 });
 
-// create model from schema
 mongoose.model('User', userSchema);
